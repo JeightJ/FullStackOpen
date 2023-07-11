0352cb775d14fac6494959283a8543f7192dec55
eightJ <joutsalainenjyrki@gmail.com>
2023-07-11 23:19:37 +0300

    Tehtävät 3.1 - 3.8 sisältä commit

--- osa2/puhelinluettelo/src/services/Persons.js
+++ osa2/puhelinluettelo/src/services/Persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -15,6 +15,7 @@ const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
+
 const remove = (id) => {
   return axios.delete(`${baseUrl}/${id}`)
 }
